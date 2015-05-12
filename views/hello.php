d053f087cb8ab2524761f999061c8590ddf1b827
Florian <paypal.xolf@yahoo.com>
2015-05-12 21:29:27 +0200

    Added a landing page

--- views/hello.php
+++ views/hello.php
@@ -1,8 +1,28 @@
-<hlmt>
+<!DOCTYPE html>
+<html>
 <head>
-	<title>Hello!</title>	
+	<title>Come in!</title>
+	<style>
+	@import url(http://fonts.googleapis.com/css?family=Lato:300);
+	@import url(http://fonts.googleapis.com/css?family=Bilbo+Swash+Caps);
+	body{
+		font-family: 'Lato', sans-serif;
+		color: #ecf0f1;
+		text-align: center;
+		background: #27ae60;
+		font-size: 50px;
+	}
+	.dreamcoil{
+		font-family: 'Bilbo Swash Caps', cursive;
+		font-size: 125%;
+		font-weight: 600;
+	}
+	</style>
 </head>
 <body>
-	<p>Hello!</p>
+	<h1>Come closer ...</h1>
+	<p>Yeah that's right, 'cause I gonna tell you a secret called "<span class="dreamcoil">Dreamcoil</span>".<br>
+	A simple PhP Framework for all the smooth people on the blue planet.</p>
+	Enjoy using <span class="dreamcoil">Dreamcoil</span> on your projects
 </body>
-</hlmt>
+</html>
