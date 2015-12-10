f2f30409de706370d33dd92ea3e3d90e491f701d
dashomeradio <dashomeradio@outlook.de>
2015-12-10 21:09:57 +0100

    Update ConstantTest.php

--- tests/ConstantTest.php
+++ tests/ConstantTest.php
@@ -5,5 +5,15 @@ class ConstantTest extends PHPUnit_Framework_TestCase
     public function testTime()
     {
         $this->assertEquals(ONE_SECOND, 1);
+        
+        $this->assertEquals(ONE_MINUTE, 60);
+        
+        $this->assertEquals(ONE_HOUR, 3600);
+        
+        $this->assertEquals(ONE_DAY, 86400);
+        
+        $this->assertEquals(ONE_WEEK, 604800);
+        
+        $this->assertEquals(ONE_YEAR, 30585600);
     }
 }
