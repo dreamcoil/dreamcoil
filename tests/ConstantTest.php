c5b6848156e4ec8eaf0081c8a1bbc5b86edbccaa
dashomeradio <dashomeradio@outlook.de>
2015-12-12 11:58:59 +0100

    Still fixing for #2

--- tests/ConstantTest.php
+++ tests/ConstantTest.php
@@ -4,21 +4,21 @@ class ConstantTest extends PHPUnit_Framework_TestCase
 {
     public function testTime()
     {
-        $this->assertEquals(ONE_SECOND, 1);
+        $this->assertEquals('ONE_SECOND', 1);
         
-        $this->assertEquals(ONE_MINUTE, 60);
+        $this->assertEquals('ONE_MINUTE', 60);
         
-        $this->assertEquals(ONE_HOUR, 3600);
+        $this->assertEquals('ONE_HOUR', 3600);
         
-        $this->assertEquals(ONE_DAY, 86400);
+        $this->assertEquals('ONE_DAY', 86400);
         
-        $this->assertEquals(ONE_WEEK, 604800);
+        $this->assertEquals('ONE_WEEK', 604800);
         
-        $this->assertEquals(ONE_YEAR, 30585600);
+        $this->assertEquals('ONE_YEAR', 30585600);
     }
     
     public function testVersion()
     {
-        $this->assertTrue(DREAMCOIL_VERSION !== null);
+        $this->assertTrue('DREAMCOIL_VERSION' !== null);
     }
 }
