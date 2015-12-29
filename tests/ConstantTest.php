b4b34be03488b63a43f0da0215d7ebc1141c9f43
Lukas Mueller <dashomeradio@outlook.de>
2015-12-29 20:59:02 +0100

    Fixing
    This commit should fix our problem with unknown constants.

--- tests/ConstantTest.php
+++ tests/ConstantTest.php
@@ -1,29 +1,31 @@
 <?php
 
+use Dreamcoil\Getter;
+
 class ConstantTest extends PHPUnit_Framework_TestCase
 {
     
 	public function __construct()
 	{
 		
-		error_reporting(E_ALL ^ E_NOTICE);
+		$getter = new Getter();
 	
 	}
     
     public function testTime()
     {
     	
-        $this->assertEquals('ONE_SECOND', 1);
+        $this->assertEquals($getter->constant('ONE_SECOND'), 1);
         
-        $this->assertEquals('ONE_MINUTE', 60);
+        $this->assertEquals($getter->constant('ONE_MINUTE'), 60);
         
-        $this->assertEquals('ONE_HOUR', 3600);
+        $this->assertEquals($getter->constant('ONE_HOUR'), 3600);
         
-        $this->assertEquals('ONE_DAY', 86400);
+        $this->assertEquals($getter->constant('ONE_DAY'), 86400);
         
-        $this->assertEquals('ONE_WEEK', 604800);
+        $this->assertEquals($getter->constant('ONE_WEEK'), 604800);
         
-        $this->assertEquals('ONE_YEAR', 30585600);
+        $this->assertEquals($getter->constant('ONE_YEAR'), 30585600);
     }
     
     public function testVersion()
