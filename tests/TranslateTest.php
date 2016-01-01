34e7ab556a5f14d70a742ed75487ae3312626af7
Lukas Mueller <dashomeradio@outlook.de>
2016-01-01 23:23:28 +0100

    Create TranslateTest.php

---
+++ tests/TranslateTest.php
@@ -0,0 +1,24 @@
+<?php
+
+use Dreamcoil\Translate;
+
+class TranslateTest extends PHPUnit_Framework_TestCase
+{
+    
+    public $translate;
+    
+	public function __construct()
+	{
+		
+		$this->translate = new Translate();
+	
+	}
+	
+	public function testValues()
+	{
+	    
+	    $this->assertEquals($this->translate->get('footer'), 'Programmiert von xolf');
+	    
+	}
+	
+}
