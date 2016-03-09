25aba4bf65e969c4d7a3e6fc529261d5ff7a0018
Florian Theimer <f.theimer@seniorbook.de>
2016-03-09 23:50:46 +0100

    REVERT

--- app/route.php
+++ app/route.php
@@ -6,4 +6,17 @@ $route = new Dreamcoil\Route;
 
 $view = new Dreamcoil\View;
 
-if($route->is('/board') $view->inc('board.show');
+if($route->is('/hello') or $route->is('/')) $view->inc('welcome.hello');
+
+if($route->is('/profile/{id}/{name}')) echo 'Hey, ' . $route->data['name'] . ':' . $route->data['id'];
+
+if($route->is('/test') or $route->is('/session')) App\ExampleClass::called();
+
+if($route->group('profile'))
+{
+
+    if($route->is('view')) echo 'view';
+
+    if($route->is('edit')) echo 'edit';
+
+}
