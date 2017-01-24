c2eccc9bf62b239beb3eb3b9d2c04cff637b75b0
xolf <xolf@users.noreply.github.com>
2017-01-24 20:26:22 +0100

    Update TranslateTest.php

--- tests/TranslateTest.php
+++ tests/TranslateTest.php
@@ -9,25 +9,19 @@ class TranslateTest extends PHPUnit_Framework_TestCase
     
 	public function __construct()
 	{
-		
 		$this->translate = new Translate();
-	
 	}
 	
 	public function testValues()
 	{
-	    
 	    $this->assertEquals($this->translate->get('fail.footer'), 'fail.footer');
 	    $this->assertEquals($this->translate->get('general.footer'), 'Programmiert von xolf');
-	    
 	}
 	
 	public function testPlaceholders()
 	{
-	    
 	    $this->assertEquals($this->translate->get('general.downloads'), 'Dreamcoil wurde schon %downloads%x heruntergeladen');
 	    $this->assertEquals($this->translate->get('general.downloads', ['downloads' => 3151]), 'Dreamcoil wurde schon 3151x heruntergeladen');
-	    
 	}
 	
 }
